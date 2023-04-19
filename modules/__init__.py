b9df976af6c1527344cb12d83fb2fe5524bfe35b
Mario González <78421275+m4riio21@users.noreply.github.com>
2023-04-19 19:39:14 +0200

    Delete modules directory

--- modules/__init__.py
+++
@@ -1,6 +0,0 @@
-from .Module import Module
-from .UploadFile import UploadFile
-from .DownloadFile import DownloadFile
-from .Screenshot import Screenshot
-from .NetInfo import NetInfo
-from .Run import Run
